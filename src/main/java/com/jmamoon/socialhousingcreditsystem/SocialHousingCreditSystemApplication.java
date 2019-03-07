c28bf7d66db0608aadc857cfff09ae403aedc2cd
Jose Arandia <jose.arandia@bolivia.assuresoft.com>
2019-03-07 10:02:23 -0400

    Basic project setup

---
+++ src/main/java/com/jmamoon/socialhousingcreditsystem/SocialHousingCreditSystemApplication.java
@@ -0,0 +1,13 @@
+package com.jmamoon.socialhousingcreditsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialHousingCreditSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialHousingCreditSystemApplication.class, args);
+	}
+
+}
