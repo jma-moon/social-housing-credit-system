11516446a19f959ac4869cac43ad4a0edd525c0f
Jose Arandia <jose.arandia@bolivia.assuresoft.com>
2019-03-08 11:37:36 -0400

    Added constants

---
+++ src/main/java/com/jmamoon/socialhousingcreditsystem/constants/Constants.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.jmamoon.socialhousingcreditsystem.constants;
+
+/**
+ *
+ * @author Jose Arandia Luna https://github.com/jma-moon
+ */
+public class Constants {
+
+    public static final String rulesPath = "rules/";
+    public static final int MONTHS = 12;
+    public static final int LEGAL_ADULT_AGE = 18;
+    public static final int BREAKPOINT_AGE = 30;
+    public static final int MAX_AGE = 60;
+    public static final int YEARS_TO_PAY_YOUNG_PERSON = 30;
+    public static final int YEARS_TO_PAY_OLD_PERSON = 25;
+
+}
