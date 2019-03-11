7f28360461d99fce1d5ad6a66a8d2eaa4ea62656
Jose Arandia Luna <jose.arandia.luna@mail.com>
2019-03-11 03:06:04 -0400

    Added basic error handling

---
+++ src/main/java/com/jmamoon/socialhousingcreditsystem/config/errorhandling/RestExceptionHandler.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.jmamoon.socialhousingcreditsystem.config.errorhandling;
+
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+/**
+ *
+ * @author Jose Arandia Luna https://github.com/jma-moon
+ */
+@Order(Ordered.HIGHEST_PRECEDENCE)
+@ControllerAdvice
+public class RestExceptionHandler extends ResponseEntityExceptionHandler {
+
+   @Override
+   protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+       String error = "Malformed JSON request";
+       return buildResponseEntity(error, HttpStatus.BAD_REQUEST);
+   }
+
+   private ResponseEntity<Object> buildResponseEntity(String error, HttpStatus httpStatus) {
+       return new ResponseEntity<>(error, httpStatus);
+   }
+
+}
